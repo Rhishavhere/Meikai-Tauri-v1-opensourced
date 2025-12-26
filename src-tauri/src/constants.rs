2379e40c4b938df8e79744dc3acfd07646fbd8dc
Rhishavhere <rhishh.me@gmail.com>
2025-12-27 01:49:05 +0530

    refactor lib - constants

---
+++ src-tauri/src/constants.rs
@@ -0,0 +1,8 @@
+/// Height of the custom title bar in pixels
+pub const TITLE_BAR_HEIGHT: f64 = 20.0;
+
+/// Default window width in pixels
+pub const WINDOW_WIDTH: f64 = 1400.0;
+
+/// Default window height in pixels
+pub const WINDOW_HEIGHT: f64 = 800.0;
